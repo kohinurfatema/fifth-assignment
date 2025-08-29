1ae7fbe002808a49a993007b79b920de4b5189a3
kohinurfatema <kohinurfatema35@gmail.com>
2025-08-29 07:16:37 -0700

    coins and call buttons done

--- script.js
+++ script.js
@@ -11,14 +11,79 @@ for(const btn of heartBtns){
     btn.addEventListener("click",function(e){
         count++;
         heartCount.textContent = count;
-        console.log(count);
     })
 }
 
 
 
+
 // -------------------------
-// COPY BUTTONS (count + copy)
+// COINS + CALL BUTTONS
+// -------------------------
+let coins = 100;
+const coinCount = document.getElementById("coin-count");
+const callHistory = document.getElementById("call-history");
+const callButtons = document.querySelectorAll(".call-btn");
+
+coinCount.textContent = coins;
+
+
+callHistory.innerHTML = "";
+
+for (const btn of callButtons) {
+  btn.addEventListener("click", function () {
+    const card = btn.closest(".card");
+    const serviceName = card.querySelector(".service-name").textContent;
+    const serviceNumber = card.querySelector(".service-number").textContent;
+
+    
+    if (coins < 20) {
+      alert("Not enough coins! You need at least 20 coins.");
+      return;
+    }
+
+    
+    coins -= 20;
+    coinCount.textContent = coins;
+
+    
+    alert("Calling " + serviceName + " at " + serviceNumber);
+
+    
+    const timeString = new Date().toLocaleTimeString([], { 
+      hour: '2-digit', 
+      minute: '2-digit', 
+      hour12: true 
+    });
+
+    
+    const li = document.createElement("li");
+    li.className = "flex justify-between items-center p-2";
+
+    
+    const infoDiv = document.createElement("div");
+    infoDiv.innerHTML = `
+      <p class="font-bold">${serviceName}</p>
+      <p class="text-gray-600 text-sm">${serviceNumber}</p>
+    `;
+
+  
+    const timeEl = document.createElement("span");
+    timeEl.textContent = timeString;
+    timeEl.className = "text-black text-xs";
+
+    li.appendChild(infoDiv);
+    li.appendChild(timeEl);
+    callHistory.appendChild(li);
+  });
+}
+
+
+
+
+
+// -------------------------
+// COPY BUTTONS 
 // -------------------------
 let copyCount = 2;
 
